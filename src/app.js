0b3109b60e5dd5dad8015ad0d0e33da899ed9247
Debashish mallick <mdebashish543@gmail.com>
2025-02-27 20:01:46 +0530

    multiple-route-handlers

--- src/app.js
+++ src/app.js
@@ -2,17 +2,51 @@ const express = require("express");
 
 const app = express();
 
-app.use("/",(req,res)=>{
-    res.send("hello from the home pages")
-})
+// app.use("/data",(req,res)=>{
+//     res.send("hello from the data")
+// })
 
-app.use("/data",(req,res)=>{
-    res.send("hello from the data")
-})
+app.get(
+  "/user",
+  [(req, res,next) => {
+    console.log("handel 1st routing");
+    // res.send("response");
+    next()
+  },
+  (req, res,next) => {
+    console.log("handel 2nd routing");
+    // res.send("2nd response");
+    next()
+  },
+  (req, res,next) => {
+    console.log("handel 3rd routing");
+    // res.send("3rd response");
+    next()
+  },
+  (req, res,next) => {
+    console.log("handel 4th routing");
+    // res.send("4th response");
+    next()
+  },
+  (req, res,next) => {
+    console.log("handel 5th routing");
+    res.send("5th response");
+    
+  },]
+);
 
-app.use("/test",(req,res)=>{
-    res.send("hello from the server for test")
-})
+// app.post("/user",(req,res)=>{
+//     res.send("data susscessfully save in DB")
+// })
 
+// app.delete("/user",(req,res)=>{
+//     res.send("delete susscessfully in DB")
+// })
 
-app.listen(2001,()=>{console.log("server is sucssesful runing on 2001")})
+// app.use("/test",(req,res)=>{
+//     res.send("hello from the server for test")
+// })
+
+app.listen(2001, () => {
+  console.log("server is sucssesful runing on 2001");
+});
