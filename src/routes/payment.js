a4130a9e498ce1fd0794a72da22b7e677fd6b176
Debashish mallick <mdebashish543@gmail.com>
2025-05-17 21:02:19 +0530

    bug-fix

--- src/routes/payment.js
+++ src/routes/payment.js
@@ -50,7 +50,7 @@ paymentRouter.post("/payment/create", userAuth, async (req, res) => {
 
 paymentRouter.post("/payment/webhook", async (req, res) => {
   try {
-    const webhookSignature = req.get["x-razorpay-signature"];
+    const webhookSignature = req.get("x-razorpay-signature");
     const isWebhookValid = validateWebhookSignature(
       JSON.stringify(req.body),
       webhookSignature,
